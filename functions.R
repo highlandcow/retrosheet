f3a56c53cbe7ba0796a271c912bd3086fcd619fa
highlandcow <johnjfarrell@gmail.com>
2020-06-04 21:41:53 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -3,3 +3,8 @@ findPlayerOnBase <- function(annualData, playerID) {
   playerOnBase <- subset(annualData, BASE1_RUN_ID == playerID | BASE2_RUN_ID == playerID | BASE3_RUN_ID == playerID )
   return(playerOnBase)
 }
+
+# Collect a team's home and away games (don't use for aggregate team data...)
+getTeamGames <- function(teamID, eventData) {
+  eventData[substr(eventData$GAME_ID, 1, 3) == teamID  | eventData$AWAY_TEAM_ID == teamID, ]
+}
