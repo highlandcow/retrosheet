f927c72f485a5d1b12d6448f477c95c5aabcaf2b
highlandcow <johnjfarrell@gmail.com>
2020-06-04 21:37:17 -0400

    Update expandStolenBases.R

--- expandStolenBases.R
+++ expandStolenBases.R
@@ -78,14 +78,28 @@ expandStolenBases <- function(eventData) {
   return(steals)
 }
 
+
+
 sumSteals <- function(stealsData, playerID) {
-  # Get the total stolen bases by a player
-    return(nrow(stealsData[stealsData$STOLE_2B_ID == playerID, ]) +
-    nrow(stealsData[stealsData$STOLE_3B_ID == playerID, ]))
+  # Returns the total number of steals by a player
+  return(nrow(stealsData[stealsData$STOLE_2B_ID == playerID, ]) +
+           nrow(stealsData[stealsData$STOLE_3B_ID == playerID, ]))
 }
 
-getBaseStealers <- function (stealsData) {
+sumStealsDetailed <- function(stealsData, playerID) {
+  # Returns data frame showing number of steals of each base and the total number of steals for a given player
+  playerSteals <- (subset(stealData, stealData$STOLE_2B_ID == playerID | stealData$STOLE_3B_ID == playerID ,))
+  stole2B <- nrow(stealsData[stealsData$STOLE_2B_ID == playerID, ])
+  stole3B <- nrow(stealsData[stealsData$STOLE_3B_ID == playerID, ])
+  stoleH <- nrow(stealsData[stealsData$STOLE_H_ID == playerID, ])
+  totalSteals <- sum(stole2B, stole3B, stoleH)
+  steals <- data.frame(STOLE_2B=stole2B, STOLE_3B=stole3B, STOLE_H=stoleH, TOTAL_STEALS=totalSteals)
+  return(steals)
+}
+
+getBaseStealers <- function(stealsData) {
   # Returns vector of players who stole at least one base
-  baseStealers <- c(unique(as.character(unique(stealsData$STOLE_2B_ID))), unique(as.character(unique(stealsData$STOLE_3B_ID))))
+  baseStealers <- c(unique(as.character(unique(stealsData$STOLE_2B_ID))), 
+                    unique(as.character(unique(stealsData$STOLE_3B_ID))))
   return(baseStealers[baseStealers != "NA"])
 }
