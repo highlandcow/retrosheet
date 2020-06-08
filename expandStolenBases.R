fe4a7ae44949e7b5f3617bfbfa726702d71b4527
highlandcow <johnjfarrell@gmail.com>
2020-06-08 14:58:19 -0400

    Update expandStolenBases.R

--- expandStolenBases.R
+++ expandStolenBases.R
@@ -1,6 +1,66 @@
-# This function expands retrosheet event data to more easily analyze stolen bases. 
-# It takes retrosheet event data and returns a data frame defining which bases were 
-# stolen during a play and the retrosheet ID of any player who stole a base. 
+# expandStolenBases() uses Retrosheet play-by-play data to produce a data frame containing details
+# for any stolen base occuring during an event. Specifically, expandStolenBases() parses the EVENT_TX 
+# value for each event and determines if a stolen base occurred. If a stolen base occurred, the
+# base stolen and the ID of the player who stole the base is captured. "NA" indicates a steal did not
+# occur. 
+
+# Call expandStolenBases() passing a data frame containing the Retrosheet play-by-play event data. 
+# The function requires the variables EVENT_TX, BASE1_RUN_ID, BASE2_RUN_ID, and BASE3_RUN_ID to work.
+
+steals1998 <- expandStolenBases(events1998)
+
+# Example output:
+head(steals1998[steals1998$BASE_STOLEN != 'NONE', ])
+
+#     BASE_STOLEN STOLE_2B_ID STOLE_3B_ID STOLE_H_ID
+# 79           2B    jeted001          NA         NA
+# 132          2B    knobc001          NA         NA
+# 177       2B+3B    vizqo001    loftk001         NA
+# 186          2B    holld001          NA         NA
+# 219          2B    erstd001          NA         NA
+# 241          2B    vizqo001          NA         NA
+
+# This can be merged into the Retrosheet play-by-play data to facilitate analysis of plays involving
+# stolen bases. Several useful functions are included in this file to explore the data. 
+
+# Example 1 - View plays in which second and home were stolen
+steals1998[steals1998$BASE_STOLEN == '2B+H', ]
+
+# Example 2 - View plays featuring a "straight steal" of home
+steals1998[steals1998$BASE_STOLEN == 'H', ]
+
+# Example 3 - View plays in which Rickey Henderson stole a base
+getPlayerSteals(steals1998, 'hendr001')
+
+# Example 4 - Get the total number of steals by Rickey Henderson
+sumSteals(steals1998, 'hendr001')
+
+# Example 5 - Summarize Rickey Henderson's steals by base stolen
+sumStealsDetailed(steals1998, 'hendr001')
+
+# Output:
+#   PLAYER_ID STOLE_2B STOLE_3B STOLE_H TOTAL_STEALS
+# 1  hendr001       51       15       0           66
+
+# Example 6 - Get a vector of playerIDs with stolen bases
+stealers1998 <- getBaseStealers(steals1998)
+
+# Example 7 - Summarize stolen bases for a vector of playerIDs
+steals_summary1998 <- sumStealsDetailedAll(steals1998, stealers1998)
+head(steals_summary1998[order(-steals_summary1998$TOTAL_STEALS), ])
+
+# Output:
+#     PLAYER_ID STOLE_2B STOLE_3B STOLE_H TOTAL_STEALS
+# 23   hendr001       51       15       0           66
+# 73   womat001       49        9       0           58
+# 53   loftk001       38       16       0           54
+# 16   stews002       45        6       0           51
+# 228  biggc001       39       11       0           50
+# 33   rodra001       30       15       1           46
+
+
+# ~~~ #
+
 
 expandStolenBases <- function(eventData) {
   i = 1
