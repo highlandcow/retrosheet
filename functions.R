f1a3fe133109da0cacfbe46744134592ba447cd8
highlandcow <johnjfarrell@gmail.com>
2020-06-18 18:03:22 -0400

    Update functions.R

--- functions.R
+++ functions.R
@@ -35,6 +35,42 @@ expandGameInfo <- function (eventData) {
 }
 
 
+### 
+
+expandBaseOutState <- function(eventData) {
+  # Returns the starting base-out state in 1B2B3B_out format
+  # Example: 000_0 reflects bases empty, 0 outs; 103_2 is 1st and 3rd with 2 outs
+  
+  base1 <- eventData$BASE1_RUN_ID != '' 
+  base2 <- eventData$BASE2_RUN_ID != ''
+  base3 <- eventData$BASE3_RUN_ID != ''
+  outs <- eventData$OUTS_CT
+  
+  base_out <- as.character()
+  
+  if ( base1 ) {
+    base_out <- '1'
+  } else {
+    base_out <- '0'
+  }
+  
+  if ( base2 ) {
+    base_out <- c(base_out,'2')
+  } else {
+    base_out <- c(base_out,'0')
+  } 
+  
+  if ( base3 ) {
+    base_out <- c(base_out,'3')
+  } else {
+    base_out <- c(base_out,'0')
+  }
+  
+  paste(c(base_out,'_',outs), collapse='')
+}
+
+
+
 
 # Collect a team's home and away games (don't use for aggregate team data...)
 getTeamGames <- function(teamID, eventData) {
