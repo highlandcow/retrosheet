df220f88f412b18b0681c36787fd35c02fe42f17
highlandcow <johnjfarrell@gmail.com>
2020-06-19 10:10:33 -0400

    Update expandStolenBases.R

--- expandStolenBases.R
+++ expandStolenBases.R
@@ -1,5 +1,5 @@
 expandStolenBases <- function(eventData) {
-  # Returns data frame with variables STOLE_2B, STOLE_3B, STOLE_H
+  #' Returns data frame with variables STOLE_2B, STOLE_3B, STOLE_H
   events <- events1998[,"EVENT_TX"]
   stole_2B <- ifelse(grepl("SB2", events), as.character(events1998[,"BASE1_RUN_ID"]), '')
   stole_3B <- ifelse(grepl("SB3", events), as.character(events1998[,"BASE2_RUN_ID"]), '')
@@ -9,7 +9,7 @@ expandStolenBases <- function(eventData) {
 }
 
 sumPlayerSteals <- function(stealsData, retroID) {
-  # Returns total steals by a player
+  #' Returns total number of steals by a player
   return(length(stealsData[stole_2B==retroID,"STOLE_2B"])
          +length(stealsData[stole_3B==retroID,"STOLE_3B"])
          +length(stealsData[stole_H==retroID,"STOLE_H"]))
