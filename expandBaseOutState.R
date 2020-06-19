111033a115f7d9a7fa40cdd8532a4407887e79d6
highlandcow <johnjfarrell@gmail.com>
2020-06-18 21:14:45 -0400

    Update expandBaseOutState.R

--- expandBaseOutState.R
+++ expandBaseOutState.R
@@ -1,6 +1,6 @@
 expandBaseOutState <- function(eventData) {
   # Returns the starting base-out state in 1B2B3B_out format
-  # Example: 000_0 reflects bases empty, 0 outs; 103_2 is 1st and 3rd with 2 outs
+  # Example: 000_0 reflects bases empty 0 outs; 103_2 is 1st and 3rd with 2 outs
   
   status1B <- ifelse(eventData[,"BASE1_RUN_ID"] =="", 0, 1)
   status2B <- ifelse(eventData[,"BASE2_RUN_ID"] =="", 0, 2)
