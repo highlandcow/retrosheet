8a2caeb66a330dd77513d2b350089cb5ae097b9b
highlandcow <johnjfarrell@gmail.com>
2020-06-22 16:08:53 -0400

    Delete expandBaseRunning.R

--- expandBaseRunning.R
+++
@@ -1,6 +0,0 @@
-expandNumRunners <- function(eventData) {
-  #' Returns vector with numbers of runners on during a play
-  events <- eventData[, c("BASE1_RUN_ID", "BASE2_RUN_ID", "BASE3_RUN_ID")]
-  runnersOn <- (eventData$BASE1_RUN_ID != "") + (eventData$BASE2_RUN_ID != "") + (eventData$BASE3_RUN_ID != "")
-  return(runnersOn)
-}
